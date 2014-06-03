43d774dd5a8e73f60925710f136482b95506c025
Pelle Almquist <per.almquist@gmail.com>
2014-06-03 17:49:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node validation roundup
+Comparison between different object validation tools for Node.js
+
+* revalidator
+* express-schema-validator
+* paperwork
+* JSV
---
+++ test.js
@@ -0,0 +1,43 @@
+var someObject = {
+  a:"one",
+  b:2,
+  c: {
+    a: [3],
+    b: {},
+    c: [],
+  }
+};
+
+var JSVenv = require('JSV').JSV.createEnvironment();
+
+var validators = {
+  JSV: function () {
+    var validator = JSVenv;
+    validator.validate(someObject, {type: 'object', properties: {a: {type: "string"}}});
+  },
+
+  revalidator: function () {
+    var validator = require('revalidator');
+    validator.validate(someObject, {properties: {a: {type: "string"}}});
+  },
+
+  paperwork: function () {
+    var validator = require('paperwork');
+    validator({a: String}, someObject, function(err, result) {});
+  },
+
+  esv: function () {
+    var validator = require('express-schema-validator');
+    validator.validate(someObject, {a: {type: String}}, function(err, result){});
+  }
+};
+
+for(v in validators) {
+  process.stdout.write('Running test on ' + v + ', ');
+  console.time('took');
+  for(var i=0; i<1000; i++) {
+    validators[v]();
+  }
+  console.timeEnd('took');
+}
+
